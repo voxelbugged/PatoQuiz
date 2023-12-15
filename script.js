fcde42129f2d983eb6e90419a9ba762bb46a5125
voxelbugged <47631415+voxelbugged@users.noreply.github.com>
2023-12-15 19:30:15 +0100

    wincyj pytyń

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const latwe = [
     ['Dokończ modlitwę: "Odpuść nam nasze winy, jako i my odpuszczamy naszym..."', 'winowajcom', 'piwowajcom', 'nauczycielom', 'Ja nie odpuszczam nikomu!!!'],
     ['Gdzie znajduje się Sosnowski Park Narodowy?', 'Nie ma takiego parku', 'Tam gdzie Sosnowiec', 'W woj. lubelskim', 'W woj. irkuckim'],
     ['Kim był Jan Paweł I dla Jana Pawła II?', 'Poprzednikiem', 'Następcą', 'Przestępcą', 'Kuzynem'],
-    ['Co obchodzimy 3 maja?', 'Uchwalenie konstytucji 3 maja', 'Dzień wolny od dnia wolnego od pracy', 'Święto flagi', 'Urodziny Wasilija Wołodina']
+    ['Co obchodzimy 3 maja?', 'Uchwalenie konstytucji 3 maja', 'Dzień wolny od dnia wolnego od pracy', 'Święto flagi', 'Urodziny Wasilija Wołodina'],
+    ['Kto napisał Pana Tadeusza?', 'Adam Mickiewicz', 'Pan Wołodyjowski', 'Pan Tadeusz', 'Henryk Sienkiewicz'],
+    ['Kto jest wokalistą polskiego zespołu Akcent?', 'Zenon Martyniuk', 'James Hetfield', 'Karol Wojtyła', 'Joe Hawley'],
+    ['Dokończ przysłowie: "Gdzie diabeł nie może..."', 'tam babę pośle', 'tam Tucka pomoże', 'to Zachodnie Pomorze', 'Jezus dosięgnie'],
+    ['Jak ma na imię Syn Boży?', 'Jezus Chrystus', 'Jezus Chytrus Superstar', 'Abraham', 'Abrakadabra']
 ];
 const srednie = [
     ['Jak ma na imię voxelbugged, twórca tej zacnej gry?', 'Jędrzej Kawa', 'Macij Hawyr', 'Andrzej Harkot', 'Jorge Mario Bergoglio'],
@@ -12,15 +16,23 @@ const srednie = [
     ['Jak miał na nazwisko Wolter?', 'Arouet', 'Wolter', 'White', 'François-Marie'],
     ['Jak nazywają się dwa koty voxela?', 'Pusia i Zenia', 'Kicikici i Belzebub Imperator', 'Mruczek i Miałczek', 'voxelbugged ma jednego kota'],
     ['Co oznacza skrót SPD?', 'Sozialdemokratische Partei Deutschlands', 'Sozialnational Partei Demokratische', 'spadaj', 'Polskie Stowarzyszenie Dyslektyków'],
-    ['Kim z zawodu był Theodore John Kaczynski?', 'Matematykiem', 'Islamskim terrorystą', 'Liderem okręgu przemysłowego', 'Bratem Jarosława Kaczyńskiego']
+    ['Kim z zawodu był Theodore John Kaczynski?', 'Matematykiem', 'Islamskim terrorystą', 'Liderem okręgu przemysłowego', 'Bratem Jarosława Kaczyńskiego'],
+    ['Jak ma na pierwsze imię krzesło Juliusz dąb sonoma?', 'Placuch', 'Racuch', 'Mbambarabambavambahambalamba', 'idk'],
+    ['Kto napisał Adama Mickiewicza?', 'Nikt', 'Pan Tadeusz', 'Dziady', 'Sofokles'],
+    ['Kto napisał piosenkę "Narkotik Kal"?', 'Hard Bass School', 'DJ Blyatman', 'Arka Noego', 'Rosyjski faraon Adidaseusz'],
+    ['Gdzie odbyła się bitwa pod Grunwaldem?', 'pod Grunwaldem', 'w 1410', 'w 1140', 'w 1989']
 ];
 const trudne = [
-    ['Dlaczego voxel jest wielkim fanem zakolanówek?', 'Bo mają trzy paski, jak Adidas', 'Bo jest technikiem programistą', 'Bo jest femboyem', 'Bo jest sponsorowany przez ich producenta'],
+    ['Dlaczego voxelbugged jest wielkim fanem zakolanówek?', 'Bo mają trzy paski, jak Adidas', 'Bo jest technikiem programistą', 'Bo jest femboyem', 'Bo jest sponsorowany przez ich producenta'],
     ['Jak poprawnie zagrać tzw. "The Lick" na pianinie?', 'A B C D B G A', 'E E E E E E', 'G A C A E E D', 'G E E F D D'],
     ['Który król stworzył Państwo Kościelne?', 'Pepin III Krótki', 'Państwo kościelne założył papież', 'Peppino Spaghetti', 'Karol I Wielki'],  
     ['Jak nazywał się nauczyciel w brytyjskim sitcomie pt. "Mind Your Language"?', 'Jeremy Brown', 'Grzegorz Braun', 'George White', 'Walter White'],
     ['Jaki jest wzór na pole powierzchni koła?', 'πr^2', '2hπ', 'π(w/o)^2', '2r^π'],
-    ['Jaką grą voxelbugged wygrał Axxess Game Jam 2022?', 'Poltergeist Heist', 'Color Plane', 'Właśnie tą', 'Grand Theft Auto: San Escobar']
+    ['Jaką grą voxelbugged wygrał Axxess Game Jam 2022?', 'Poltergeist Heist', 'Color Plane', 'Właśnie tą', 'Grand Theft Auto: San Escobar'],
+    ['Co było przyczyną sporu między Antygoną a Kreonem?', 'Pochowanie Polinika', 'Pochowanie Sofoklesa', 'Wypicie ostatniego piwa w Tebach', 'Spór był między Antygoną a Winicjuszem'],
+    ['Kto jest wokalistą rumuńskiego zespołu Akcent?', 'Adrian Sînă', 'Zenon Martyniuk', 'Klaus Iohannis', 'Wruk'],
+    ['Jaki ciąg znaków nie spełnia wyrażenia regularnego [A-Z][a-z]{3,}?', 'Nie wiadomo', 'Janusz', 'Ufhund./3GUfj111,2iu', 'Katowice'],
+    ['Kim był Ksiądz Robak dla Jacka Soplicy?', 'sobą', 'ojcem', 'kuzynem', 'sprzedawcą w monopolowym']
 ];
 
 
